7574ce9aa2dd8a03537aeca4e2f1cf9604d5224b
Kuzmin Yaroslav <kuzmin.yaroslav@gmail.com>
2016-01-01 13:38:52 +0500

    Н

--- server/server_modbus.c
+++ server/server_modbus.c
@@ -17,44 +17,54 @@
 
 
 char ip_address[] = "127.0.0.1";
-uint16_t port = 502;
+uint16_t port = 1502;
 uint16_t id_server = 0;
 
 
-uint16_t begin_registers = 0x100;
-uint16_t amount_registers = 32;
+uint16_t begin_registers = 0x1064;
+uint16_t amount_registers = 0x100;
+
+static int set_value_registers(modbus_mapping_t * mb)
+{
+	return SUCCESS;
+}
 
 int main(int argc,char * argv[])
 {
 	int rc;
+	char * name_file;
 	int socket;
 	modbus_t *ctx;
 	modbus_mapping_t *mb_mapping;
 	uint8_t * query;
 	uint16_t registers = 0;
-	/*int header_length;*/
 
 	if(argc != 2){
-		g_printf("Нет номера сервера!\n");
+		g_printf("Нет имени конфигурационного файла!\n");
 		return SUCCESS;
 	}
+	name_file = argv[1];
+	g_printf("Файл конфигурации : %s\n",name_file);
 
-	id_server = g_ascii_strtoll(argv[1],NULL,10);
-	if(id_server == 0){
-		g_printf("Некорректный номер сервера : %s\n",argv[1]);
-		return SUCCESS;
-	}
+	mb_mapping = modbus_mapping_new(0,0,begin_registers + amount_registers,0);
+	if(mb_mapping)
+	rc = init_config();
+
+	return SUCCESS;
 
 	ctx = modbus_new_tcp(ip_address, port);
 	query = g_malloc0(MODBUS_TCP_MAX_ADU_LENGTH);
 	/*загаловок разный у TCP и RTU*/
 	/*header_length = modbus_get_header_length(ctx);*/
 	modbus_set_debug(ctx,FALSE);
-	mb_mapping = modbus_mapping_new(0,0,begin_registers + amount_registers,0);
 
-	g_printf(" Старт сервера : ID %d\n",id_server);
+	g_printf(" Старт сервера : port %d : ID %d\n",port,id_server);
 
 	socket = modbus_tcp_listen(ctx, 1);
+	if(socket == -1){
+		g_printf("Несмог создать соединение!\n");
+		return SUCCESS;
+	}
 
 	modbus_tcp_accept(ctx, &socket);
 	g_printf(" Клиент подключился к серверу!");
@@ -65,9 +75,10 @@ int main(int argc,char * argv[])
 			g_printf("Клиент закрыл соединение!\n");
 			break;
 		}
-		registers ++;
-		mb_mapping->tab_registers[0x100] = registers;
-		rc = modbus_reply(ctx, query, rc, mb_mapping);
+
+		set_value_registers(mb_mapping);
+
+		rc = modbus_reply(ctx,query,rc,mb_mapping);
 		if (rc == -1) {
 			g_printf("Клиент закрыл соединение!\n");
 			break;
