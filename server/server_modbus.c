af9bd76646f2538477de3b89e2395b43003e5226
Kuzmin Yaroslav <kuzmin.yaroslav@gmail.com>
2016-06-27 07:28:14 +0500

    Изменения

--- server/server_modbus.c
+++ server/server_modbus.c
@@ -31,8 +31,9 @@ uint16_t re_encoder_horizontal   = 0x1068;
 uint16_t re_pressure             = 0x1069;
 uint16_t re_amperage_vertical    = 0x106A;
 uint16_t re_amperage_horizontal  = 0x106B;
+uint16_t re_work                 = 0x106C;
 
-uint16_t re_valva                = 0x106E;
+uint16_t re_valve                = 0x106E;
 uint16_t re_tic_valve            = 0x106F;
 
 uint16_t re_sensor_fire_state    = 0x1072;
@@ -194,56 +195,228 @@ int init_config(char * name,modbus_mapping_t * mb)
 	return SUCCESS;
 }
 
+
+uint16_t tic_vertical = 0;
+uint16_t tic_horizontal = 0;
+uint16_t valve = 0;
+uint16_t valve_delay = 0;
+uint16_t pressure = 0;
+uint16_t fire_alarm_state = 1;
+uint16_t fire_alarm_state_count = 0;
+uint16_t work = 0x0100;
+uint16_t work_delay = 0;
+
 int set_value_registers(modbus_mapping_t * mb)
 {
-	/*
+ 	/*
 	бит 0 - датчик положения "ВВЕРХ"
 	бит 1 - датчик положения "ВНИЗ"
 	бит 2 - датчик положения "ВЛЕВО"
 	бит 3 - датчик положения "ВПРАВО"*/
 	mb->tab_registers[re_state_lafet] = 0x000F;
-	/*значение 0 - 30*/
-	mb->tab_registers[re_tic_vertical] = 30;
-	/*значение 0 - 60*/
-	mb->tab_registers[re_tic_horizontal] = 60;
+/*****************************************************************************/
+
+	/*значение 0 - 30  (угол от 0 до 180 градусов) */
+	tic_vertical ++;
+	if( tic_vertical > 30){
+		tic_vertical = 0;
+	}
+	mb->tab_registers[re_tic_vertical] = tic_vertical;
+/*****************************************************************************/
+
+	/*значение 0 - 60 (угол от 0 до 360 грудусав)*/
+	tic_horizontal ++;
+	if( tic_horizontal > 60){
+		tic_horizontal =0;
+	}
+	mb->tab_registers[re_tic_horizontal] = tic_horizontal;
+/*****************************************************************************/
+
 	/*значение 0-65535*/
 	mb->tab_registers[re_encoder_vertical] = 2000;
+/*****************************************************************************/
 	/*значение 0-65535*/
 	mb->tab_registers[re_encoder_horizontal] = 3000;
+/*****************************************************************************/
+
 	/*значение 0-65535*/
-	mb->tab_registers[re_pressure] = 4000;
+	switch(pressure){
+		case 0:
+			pressure = 1;
+			break;
+		case 1:
+			pressure = 2;
+			break;
+		case 2:
+			pressure = 3;
+			break;
+		case 3:
+			pressure = 0;
+			break;
+		default:
+			pressure = 0;
+	}
+	mb->tab_registers[re_pressure] = pressure;
+/*****************************************************************************/
 	/*значение 0-65535*/
 	mb->tab_registers[re_amperage_vertical] = 5000;
+/*****************************************************************************/
 	/*значение 0-65535*/
 	mb->tab_registers[re_amperage_horizontal] = 6000;
+/*****************************************************************************/
+	valve_delay++;
+	if(valve_delay == 10){
+		valve_delay = 0;
 	/*
 	бит 0 - датчик состояния "ОТКРЫТ"
-	бит 1 - датчик состояния "ЗАКРЫТ"*/
-	mb->tab_registers[re_valva] = 3;
+	бит 1 - датчик состояния "ЗАКРЫТ"
+	бит 2 - датчик состояния "ОТКРЫВАЕТСЯ"
+	бит 3 - датчик состояния "ЗАКРЫВАЕТСЯ"*/
+
+		switch(valve){
+			case 0:
+				valve = 0x0001;
+				break;
+			case 0x0001:
+				valve = 0x0008;
+				break;
+			case 0x0008:
+				valve = 0x0002;
+				break;
+			case 0x0002:
+				valve = 0x0004;
+				break;
+			case 0x0004:
+				valve = 0x0001;
+				break;
+			default:
+				valve = 0;
+		}
+	}
+	mb->tab_registers[re_valve] = valve;
 	/*значение 0-65535*/
+/*****************************************************************************/
 	mb->tab_registers[re_tic_valve] = 7000;
+/*****************************************************************************/
 	/*
 	бит 0 - датчик в состоянии "ПОЖАР"
 	бит 1 - датчик состояния "НЕИСПРАВНОСТЬ"*/
 	mb->tab_registers[re_sensor_fire_state] = 3;
+/*****************************************************************************/
 	/*
-	бит 0 - пожар шлейфа №1
-	бит 1 - пожар шлейфа №2
-	бит 2 - пожар шлейфа №3
-	бит 3 - пожар шлейфа №4
-	бит 4 - пожар шлейфа №5
-	бит 5 - пожар шлейфа №6
-	бит 6 - пожар шлейфа №7
-	бит 7 - пожар шлейфа №8
-	бит 8 - пожар шлейфа №9
-	бит 9 - пожар шлейфа №10
-	бит 10 - пожар шлейфа №11
-	бит 11 - пожар шлейфа №12
-	бит 12 - пожар шлейфа №13
-	бит 13 - пожар шлейфа №14
-	бит 14 - пожар шлейфа №15
-	бит 15 - пожар шлейфа №16*/
-	mb->tab_registers[re_fire_alarm_state] = 0xFFFF;
+	0 - нет пожар
+	1 - пожар
+	*/
+	fire_alarm_state_count ++;
+	if(fire_alarm_state_count == 20){
+		fire_alarm_state_count = 0;
+		if(	fire_alarm_state == 0){
+			fire_alarm_state = 1;
+		}
+		else{
+			fire_alarm_state = 0;
+		}
+
+	}
+	/*0x1074*/
+	mb->tab_registers[re_fire_alarm_state] = fire_alarm_state;
+/*****************************************************************************/
+
+	work_delay++;
+	if(work_delay == 20){
+		work_delay = 0;
+/*
+	 бит 0 - ошибка вертикальной оси
+	 бит 1 - ошибка горизонтальной оси
+	 бит 2 - ошибка привода актуатора 1
+	 бит 3 - ошибка привода актуатора 2
+	 бит 4 - ошибка привода заслонки
+	 бит 5 - резерв
+	 бит 6 - резерв
+	 бит 7 - резерв
+	 бит 8 - автоматический режим работы
+	 бит 9 - ручной режим работы (управление местное)
+	 бит 10 - ручной режим работы (управление оператора)
+	 бит 11 - режим тестирования
+	 бит 12 - режим  горизонтальной осцилляции
+	 бит 13 - режим вертикальной осцилляции
+	 бит 14 - режим осцилляции SAW
+	 бит 15 - режим осцилляции STEP
+	 0 - датчик OFF, 1 - датчик ON
+ */
+	/*0x106C*/
+		switch(work){
+			case 0x0100:
+				work = 0x0101;
+				break;
+			case 0x0101:
+				work = 0x0102;
+				break;
+			case 0x0102:
+				work = 0x0104;
+				break;
+			case 0x0104:
+				work = 0x0108;
+				break;
+			case 0x0108:
+				work = 0x0110;
+				break;
+#if 0
+			case 0x0110:
+				work = 0x011f;
+				break;
+			case 0x011f:
+				work = 0x011A;
+				break;
+			case 0x011A:
+				work = 0x0119;
+				break;
+			case 0x0119:
+				work = 0x011D;
+				break;
+			case 0x011D:
+				work = 0x0200;
+				break;
+			case 0x0200:
+				work = 0x0201;
+				break;
+			case 0x0201:
+				work = 0x0202;
+				break;
+			case 0x0202:
+				work = 0x0204;
+				break;
+			case 0x0204:
+				work = 0x0208;
+				break;
+			case 0x0208:
+				work = 0x0210;
+				break;
+			case 0x0210:
+				work = 0x021f;
+				break;
+			case 0x021f:
+				work = 0x1100;
+				break;
+			case 0x1100:
+				work = 0x2100;
+				break;
+			case 0x2100:
+				work = 0x4100;
+				break;
+			case 0x4100:
+				work = 0x8100;
+				break;
+			case 0x8100:
+				work = 0x0100;
+				break;
+#endif
+			default:
+				work = 0x0100;
+		}
+	}
+	mb->tab_registers[re_work] = work;
+
 
 	return SUCCESS;
 }
